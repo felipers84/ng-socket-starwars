070eb967e6c341219f1742ca7f335f5f2b2758e7
Felipe Campos <fl.campos@gmail.com>
2019-08-02 22:49:32 -0300

    Correção na resposta do back em nodeJS

--- back/server.js
+++ back/server.js
@@ -5,7 +5,6 @@ const wss = new WebSocket.Server({ port: 8765 })
 wss.on('connection', ws => {
     ws.on('message', message => {
         console.log(`Received message => ${message}`)
-        ws.send("pong");
+        ws.send(message);
     })
-    ws.send('ho!')
 })
